aa3bf6cd419bd71928b87b7466babf5af15dbf2d
aakash30112000 <120931718+aakash30112000@users.noreply.github.com>
2023-04-18 04:08:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ def predict_result():
     si1 = [[0,0,0,0,0,0,0,0,0,0,0,0,2.0,1.30,103.0,106.0]]
     result = pickled_model.predict(si1)
 
-    s = "Still trying to predict"
+    res = "Still trying to predict"
 
     if result[0] == 0:
-        s = "Congrats! Thyroid Not Detected."
+        res = "Congrats! Thyroid Not Detected."
     else:
-        s = "Sorry! It's Thyroid."
+        res = "Sorry! It's Thyroid."
 
-    return s
+    return res
 
 
 @app.get("/")
 async def root():
-    s = predict_result()
-    return s
+    result = predict_result()
+    return result
