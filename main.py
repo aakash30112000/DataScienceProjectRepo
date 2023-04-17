b396227eab86a3b759828605542694416be653c3
aakash30112000 <120931718+aakash30112000@users.noreply.github.com>
2023-04-18 04:01:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import pickle
+import sklearn
+
+app = FastAPI()
+
+def predict_result():
+    pickled_model = pickle.load(open('C:/Users/ag301/Desktop/model.pkl', 'rb'))
+
+    si1 = [[0,0,0,0,0,0,0,0,0,0,0,0,2.0,1.30,103.0,106.0]]
+    result = pickled_model.predict(si1)
+
+    s = "Still trying to predict"
+
+    if result[0] == 0:
+        s = "Congrats! Thyroid Not Detected."
+    else:
+        s = "Sorry! It's Thyroid."
+
+    return s
+
+
+@app.get("/")
+async def root():
+    s = predict_result()
+    return s
