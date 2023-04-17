fec1bedc21b530983e6d8fc465fbc2e8f0f2014b
aakash30112000 <120931718+aakash30112000@users.noreply.github.com>
2023-04-18 04:07:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sklearn
 app = FastAPI()
 
 def predict_result():
-    pickled_model = pickle.load(open('C:/Users/ag301/Desktop/model.pkl', 'rb'))
+    pickled_model = pickle.load(open('model.pkl', 'rb'))
 
     si1 = [[0,0,0,0,0,0,0,0,0,0,0,0,2.0,1.30,103.0,106.0]]
     result = pickled_model.predict(si1)
